18983b99bbe82f7f0963e11eebe01eda1d00bdbb
tstho <ts.salmon@proton.me>
2023-05-25 10:36:52 +0200

    change display to Hello World Master

--- Multiverse/Program.cs
+++ Multiverse/Program.cs
@@ -6,7 +6,7 @@ namespace Multiverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Master!");
         }
     }
 }
