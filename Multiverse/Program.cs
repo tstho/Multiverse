5143642adbeb2dda4f4e5468404f0cbaa845ea67
tstho <ts.salmon@proton.me>
2023-05-25 10:59:40 +0200

    change display to World B!

--- Multiverse/Program.cs
+++ Multiverse/Program.cs
@@ -6,7 +6,7 @@ namespace Multiverse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World A!");
+            Console.WriteLine("Hello World B!");
         }
     }
 }
